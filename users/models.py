5bfc5ba9e7f58d8ccc4e84ce6484ef1f0a01ac61
pavel.krotkikh <pavel.krotkikh@hygge.software>
2021-05-11 11:26:25 +0300

    Model naming

--- users/models.py
+++ users/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth import get_user_model
 
 
 class User(AbstractUser):
-    friends = models.ManyToManyField('self', through='FriendshipInfo')
+    friends = models.ManyToManyField('self', through='Friendship')
 
 
-class FriendshipInfo(models.Model):
+class Friendship(models.Model):
     from_user = models.ForeignKey(User, related_name="to_user", on_delete=models.CASCADE)
     to_user = models.ForeignKey(User, related_name="from_user", on_delete=models.CASCADE)
     accepted_status = models.BooleanField(blank=True, null=True)
