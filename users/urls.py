5f904170bfc8cc4c942f5c5eb942684e13c71e2a
pavel.krotkikh <pavel.krotkikh@hygge.software>
2021-05-11 12:53:31 +0300

    Issues: My friends url #10

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from users import views
 urlpatterns = [
     path('/', views.user_list),
     path('me/', views.user_me),
-
-    path('friends/', views.friends),
+    path('me/friends/', views.friends),
+    
     path('<int:user_id>/friend_request/', views.friend_request),
     path('<int:user_id>/friend_request/accept/', views.friend_accept),
 ]
